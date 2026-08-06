8442aca4278ea12aab029edd023db88ea6d10555
mohdraziuddin08-art <mohd.raziuddin08@gmail.com>
2026-08-06 12:30:40 +0530

    day 20

---
+++ Code20.py
@@ -0,0 +1,43 @@
+'Day 20 (Questions 191–200)'
+
+
+
+'Q191. Read data from a JSON file and display its contents.'
+
+
+
+'Q192. Write a Python dictionary to a JSON file.'
+
+
+
+'Q193. Read data from a CSV file and calculate the average of a numeric column.'
+
+
+
+'Q194. Write data to a CSV file using Python.'
+
+
+
+'Q195. Create a program that logs errors to a file using the logging module.'
+
+
+
+'Q196. Create a student report card by reading marks from a CSV file and calculating the total, average, percentage, and grade.'
+
+
+
+'Q197. Create a program that backs up a text file by copying it to another location with the current date in the filename.'
+
+
+
+'Q198. Build a contact management system that stores and retrieves contact details using JSON.'
+
+
+
+'Q199. Build a to-do list application that saves tasks in a JSON file.'
+
+
+
+'Q200. Create a mini employee management system using CSV files with options to Add, View, Search, Update, and Delete employee records.'
+
+
